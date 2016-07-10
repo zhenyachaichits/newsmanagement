fef43804d515880cdf5a0a0d28029f62f864e7bd
Evgeniy Chaichits <zhenyachay@gmail.com>
2016-07-11 00:45:22 +0300

    Security fix

--- news-admin/src/main/java/com/epam/news/admin/security/service/UserDetailsServiceImpl.java
+++ news-admin/src/main/java/com/epam/news/admin/security/service/UserDetailsServiceImpl.java
@@ -1,7 +1,24 @@
 package com.epam.news.admin.security.service;
 
-/**
- * Created by Zheny Chaichits on 7/10/2016.
- */
-public class UserDetailsServiceImpl {
+import com.epam.news.common.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+
+
+        return null;
+    }
 }
